9d136b8599de5b54719beb9f595fb64d74ccc9bd
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-05-24 13:19:54 +0200

    Added About component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
         </Switch>
         </BrowserRouter>
       </div>

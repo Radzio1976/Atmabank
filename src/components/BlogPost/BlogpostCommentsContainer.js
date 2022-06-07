75588cd0825ca90e6dad640c96c337efe86eb738
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-06-07 15:44:39 +0200

    Created BlogpostCommentsFormContainer component

--- src/components/BlogPost/BlogpostCommentsContainer.js
+++ src/components/BlogPost/BlogpostCommentsContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import Axios from 'axios';
 
+import BlogpostCommentsFormContainer from './BlogpostCommentsFormContainer';
 import {BlogPostContext} from './BlogPost';
 
 const BlogpostCommentsContainer = (props) => {
@@ -23,20 +24,7 @@ const BlogpostCommentsContainer = (props) => {
 
     return(
         <div className="blogpost-comments-container">
-            <div className="blogpost-comments-form-container">
-                <div className="blogpost-comments-form-title">
-                    <h1>Zostaw komentarz</h1>
-                    {BlogPostCtx.mainCommentsFormVisibility === false ? <button onClick={BlogPostCtx.showCommentButton}>Zostaw komentarz</button> : ""}
-                </div>
-                {BlogPostCtx.mainCommentsFormVisibility === true ? <div className="blogpost-comments-form-wrapper">
-                    <form>
-                        <input type="text" name="name" value={BlogPostCtx.name} onChange={(e) => BlogPostCtx.nameChange(e.target.value)} placeholder="Imię"/>
-                        <input type="text" name="email" value={BlogPostCtx.email} onChange={(e) => BlogPostCtx.emailChange(e.target.value)} placeholder="Email" />
-                        <textarea name="text" value={BlogPostCtx.text} onChange={(e) => BlogPostCtx.textChange(e.target.value)} placeholder="Twój komentarz" />
-                        <button onClick={BlogPostCtx.sendComment}>Wyślij</button>
-                    </form>
-                </div> : ""}
-            </div>
+            <BlogpostCommentsFormContainer />
 
             <div className="blogpost-comments-wrapper">
             {

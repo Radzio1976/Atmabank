b39cc9b036c6a91008ed6c8d6b85d439b255c31f
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-06-28 17:18:43 +0200

    Changed index.js

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  //app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
   })

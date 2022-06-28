636875967ea5261052c691c60a05d6e74677def0
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-06-28 14:56:44 +0200

    changed index.js file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
   })
 }
 

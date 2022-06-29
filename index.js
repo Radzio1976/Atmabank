1efcf9786131cdde55b6eff91e02c35b54c83686
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-06-29 07:20:44 +0200

    Added mongoDBConnection func in index.js

--- index.js
+++ index.js
@@ -14,37 +14,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
-app.get("/api/", (req, res) => {
-  res.send("test");
-})
-
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get("*", function (_, res) {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"),
-  function (err) {
-    if (err) {
-      res.status(500), send(err);
-    }
-  }
-  )
-})
-
 const client = new MongoClient(uri);
 
-
-app.post("/getComments", (req, res) => {
-  async function main() {
+async function mongoDBConnection() {
   try {
-      // Connect to the MongoDB cluster
-    await client.connect();
-    await getComments(client);
+       //Connect to the MongoDB cluster
+    client.connect();
+    //getComments(client);
   } catch (error) {
     console.log("Nie udało się uzyskać połączenia z bazą danych", error);
   }
 }
-main();
+mongoDBConnection();
 
+
+app.post("/getComments", (req, res) => {
   async function getComments(client) {
     const result = await client.db("atma_bank").collection("comments").find({}).toArray();
     if (result) {
@@ -53,21 +37,11 @@ main();
         res.send(error);
     }
   }
+  getComments(client);
 })
 
 
 app.post("/addComment", (req, res) => {
-  async function main() {
-    try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-      await addComment(client, req, res);
-    }  catch (error) {
-      console.log("Nie udało się uzyskać połączenia z bazą danych", error);
-    }
-  }
-  main();
-
   async function addComment(client, req, res) {
     const comment = req.body;
     await client.db("atma_bank").collection("comments").insertOne({
@@ -93,20 +67,11 @@ app.post("/addComment", (req, res) => {
     }
     })
   }
+  addComment(client, req, res);
 })
 
 
 app.post("/addCommentsAnswer", (req, res) => {
-  async function main() {
-    try {
-      await client.connect();
-      await addCommentsAnswer(client, req, res);
-    }  catch (error) {
-      console.log("Nie udało się uzyskać połączenia z bazą danych", error);
-    }
-  }
-  main();
-
   async function addCommentsAnswer(client, req, res) {
     let commentAnswer = req.body;
     commentAnswer._id = new ObjectID();  
@@ -124,9 +89,12 @@ app.post("/addCommentsAnswer", (req, res) => {
       }
     })
     }
+    addCommentsAnswer(client, req, res);
   });
 
-  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
   /*
   app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "./client/build", "index.html"));

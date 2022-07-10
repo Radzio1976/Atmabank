97b34f327f56892e0207c7c3d0721007b69e4e1e
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2022-07-10 08:07:10 +0200

    Added useFormValidationHook

--- client/src/hooks/useSendCommentHook.js
+++ client/src/hooks/useSendCommentHook.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
 import AppState from "./AppState";
+import useFormValidationHook from "./useFormValidationHook";
 import useCurrentPostCommentsHook from "./useCurrentPostCommentsHook";
 import useCurrentPostCommentsQtyHook from "./useCurrentPostCommentsQtyHook";
 import useLastFiveCommentsHook from "./useLastFiveCommentsHook";
@@ -8,12 +9,14 @@ import useResetFormHook from "./useResetFormHook";
 
 const useSendCommentHook = () => {
     const {postID, name, email, text, currentPostSlug} = AppState();
+    const {getFormValidation} = useFormValidationHook();
     const {getCurrentPostComments} = useCurrentPostCommentsHook();
     const {getCurrentPostCommentsQty} = useCurrentPostCommentsQtyHook();
     const {getLastFiveComments} = useLastFiveCommentsHook();
     const {resetForm} = useResetFormHook();
 
-    const sendComment = () => { 
+    const sendComment = () => {
+        if (getFormValidation({name, email, text}) === true) {    
           let comment = {
             postID, 
             name, 
@@ -42,6 +45,7 @@ const useSendCommentHook = () => {
         .catch(err => {
             console.log("Nie udało się wysłać komentarza", err);
         });
+        };   
       };
 
       return {sendComment};
